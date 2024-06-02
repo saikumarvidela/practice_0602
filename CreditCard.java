c0efb3c8c5e689a24afcfafad4bb5a7ad84dd35f
sai <vavskumar@gmail.com>
2024-06-02 21:14:33 +0530

    modified creditcard in local repo

--- CreditCard.java
+++ CreditCard.java
@@ -3,6 +3,8 @@ public class CrediCard{
 public static void main(String args[]){
 System.out.println("Hello");
 System.out.println("Hello added in Github");
+System.out.println("Hello added in Local repo");
+
   
 }
 }
