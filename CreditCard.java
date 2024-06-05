c935d7818b20e4ffd2bcfbf64f6a38ecf56fbb8d
sai <vavskumar@gmail.com>
2024-06-05 23:49:13 +0530

    changes

--- CreditCard.java
+++ CreditCard.java
@@ -5,7 +5,9 @@ System.out.println("Hello");
 System.out.println("Hello added in Github");
 System.out.println("Hello added in Local repo");
 System.out.println("Hello added Branch");
-
-  
+}
+public void m2()
+{
+System.out.println("Hello added m2 method");
 }
 }
