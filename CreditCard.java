3f0ab0974b630ae4cb6d04be79978a5ec98742b2
vavskumar <62796104+saikumarvidela@users.noreply.github.com>
2024-06-05 23:44:09 +0530

    Update CreditCard.java

--- CreditCard.java
+++ CreditCard.java
@@ -5,7 +5,8 @@ System.out.println("Hello");
 System.out.println("Hello added in Github");
 System.out.println("Hello added in Local repo");
 System.out.println("Hello added Branch");
-
-  
 }
+  public void m1(){
+    System.out.println("Added Feature code changes");
+  }
 }
